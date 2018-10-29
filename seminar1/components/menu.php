277409c0b6ef2126ff88473b7a6d5be2f5115f62
LinusBF <linus.webdevelopment@gmail.com>
2018-10-29 23:02:46 +0100

    Some initial work

---
+++ seminar1/components/menu.php
@@ -0,0 +1,15 @@
+<nav class="main-menu">
+	<div id="index-page" class="menu-item"><a href="<?php echo ROOT_PATH; ?>index.php">Home</a></div>
+	<div id="calendar-page" class="menu-item"><a href="<?php echo ROOT_PATH; ?>pages/calendar.php">Calendar</a></div>
+	<div id="recipe-pages" class="menu-item dropdown-menu">
+		<p>Recipes</p>
+		<div class="dropdown-container">
+			<div id="meatballs-recipe-page" class="dropdown-item">
+				<a href="<?php echo ROOT_PATH; ?>pages/recipes/meatballs.php">Meatballs</a>
+			</div>
+			<div id="pancakes-recipe-page" class="dropdown-item">
+				<a href="<?php echo ROOT_PATH; ?>pages/recipes/meatballs.php">Pancakes</a>
+			</div>
+		</div>
+	</div>
+</nav>
--- seminar1/index.php
+++ seminar1/index.php
@@ -1,9 +1,14 @@
+<?php
+DEFINE("ROOT_PATH", "/id1354/seminar1/" );
+?>
 <html>
 <head>
+	<?php include "./components/default-head.php" ?>
 	<title>ID1354 - Seminar 1</title>
 </head>
 <body>
+	<?php include "./components/menu.php" ?>
 	<h1>Hello world</h1>
-	<?php echo "test"; ?>
+	<p><?php echo "test"; ?></p>
 </body>
 </html>
