b71a99e0dc504256520cbc728612d89414db7b76
LinusBF <linus.webdevelopment@gmail.com>
2018-10-31 00:22:01 +0100

    Added Bootstrap and basic menu and carousel

--- seminar1/components/menu.php
+++ seminar1/components/menu.php
@@ -1,15 +1,50 @@
-<nav class="main-menu">
-	<div id="index-page" class="menu-item"><a href="<?php echo ROOT_PATH; ?>index.php">Home</a></div>
-	<div id="calendar-page" class="menu-item"><a href="<?php echo ROOT_PATH; ?>pages/calendar.php">Calendar</a></div>
-	<div id="recipe-pages" class="menu-item dropdown-menu">
-		<p>Recipes</p>
-		<div class="dropdown-container">
-			<div id="meatballs-recipe-page" class="dropdown-item">
-				<a href="<?php echo ROOT_PATH; ?>pages/recipes/meatballs.php">Meatballs</a>
-			</div>
-			<div id="pancakes-recipe-page" class="dropdown-item">
-				<a href="<?php echo ROOT_PATH; ?>pages/recipes/meatballs.php">Pancakes</a>
-			</div>
+<?php
+
+function getMenu($sActive) {
+	$sActiveHTML = " <span class='sr-only'>(current)</span>";
+	$aRecipes = array(
+		array(
+			"id"   => "mballs",
+			"name" => "Meatballs",
+			"link" => ROOT_PATH . "pages/recipes/meatballs.php"
+		),
+		array(
+			"id" => "pcakes",
+			"name" => "Pancakes",
+			"link" => ROOT_PATH."pages/recipes/pancakes.php"
+		)
+	);
+	?>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="<?php echo ROOT_PATH."index.php";?>">Recipes.com</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+					   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Recipes<?php echo ($sActive === "recipe" ? $sActiveHTML : "")?>
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<?php foreach ($aRecipes as $recipe):?>
+							<a class="dropdown-item" href="<?php echo $recipe['link']; ?>">
+								<?php echo $recipe['name'];; ?>
+							</a>
+						<?php endforeach;?>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="<?php echo ROOT_PATH."pages/calendar.php";?>">
+						Calendar<?php echo ($sActive === "calendar" ? $sActiveHTML : "")?>
+					</a>
+				</li>
+			</ul>
 		</div>
-	</div>
-</nav>
+	</nav>
+	<?php
+}
+?>
--- seminar1/index.php
+++ seminar1/index.php
@@ -1,5 +1,6 @@
 <?php
 DEFINE("ROOT_PATH", getenv ("PRODUCTION") !== false ? "/seminar1/" : "/id1354/seminar1/");
+include "./components/menu.php";
 ?>
 <html>
 <head>
@@ -7,8 +8,19 @@ DEFINE("ROOT_PATH", getenv ("PRODUCTION") !== false ? "/seminar1/" : "/id1354/se
 	<title>ID1354 - Seminar 1</title>
 </head>
 <body>
-	<?php include "./components/menu.php" ?>
-	<h1>Hello world</h1>
-	<p><?php echo "test"; ?></p>
+	<?php  getMenu("index"); ?>
+	<div class="mt-3 d-flex flex-column justify-content-center">
+		<div id="recipeSlide" class="carousel slide" data-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="media/meatballs.jpg" alt="Meatballs">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="media/pancakes.jpg" alt="Pancakes">
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include "./components/default-footer.php" ?>
 </body>
 </html>
