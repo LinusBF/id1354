905741b901e1e443a1c2daf462f959753e670e89
LinusBF <linus.webdevelopment@gmail.com>
2018-10-31 22:34:26 +0100

    Changed the basic structure of the pages and started implementing a design

--- seminar1/components/menu.php
+++ seminar1/components/menu.php
@@ -15,7 +15,7 @@ function getMenu($sActive) {
 		)
 	);
 	?>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-dark">
 		<a class="navbar-brand" href="<?php echo ROOT_PATH."index.php";?>">Recipes.com</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
 				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- seminar1/index.php
+++ seminar1/index.php
@@ -1,26 +1,21 @@
 <?php
-DEFINE("ROOT_PATH", getenv ("PRODUCTION") !== false ? "/seminar1/" : "/id1354/seminar1/");
-include "./components/menu.php";
-?>
-<html>
-<head>
-	<?php include "./components/default-head.php" ?>
-	<title>ID1354 - Seminar 1</title>
-</head>
-<body>
-	<?php  getMenu("index"); ?>
-	<div class="mt-3 d-flex flex-column justify-content-center">
-		<div id="recipeSlide" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner">
-				<div class="carousel-item active">
-					<img class="d-block w-100" src="media/meatballs.jpg" alt="Meatballs">
-				</div>
-				<div class="carousel-item">
-					<img class="d-block w-100" src="media/pancakes.jpg" alt="Pancakes">
-				</div>
+DEFINE( "ROOT_PATH", getenv( "PRODUCTION" ) !== false ? "/seminar1/" : "/id1354/seminar1/" );
+include "./components/base-body.php";
+
+$pageContent = function () {
+	?>
+	<div id="recipeSlide" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="media/meatballs.jpg" alt="Meatballs">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="media/pancakes.jpg" alt="Pancakes">
 			</div>
 		</div>
 	</div>
-	<?php include "./components/default-footer.php" ?>
-</body>
-</html>
+	<?php
+};
+
+printBody($pageContent);
+?>
