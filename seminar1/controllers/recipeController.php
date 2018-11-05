441690fcd2b75d94ff0671c61820be80c6ac1ad3
LinusBF <linus.webdevelopment@gmail.com>
2018-11-05 21:29:12 +0100

    Small corrections

--- seminar1/controllers/recipeController.php
+++ seminar1/controllers/recipeController.php
@@ -8,18 +8,32 @@ function getRecipes(){
 	return $recipes['recipes'];
 }
 
+/**
+ * @return array
+ */
 function getIngredients(){
 	$recipes = json_decode(file_get_contents(APP_PATH."data/recipes.json"), true);
 	return $recipes['ingredients'];
 }
 
+/**
+ * @return int - The recipe ID of the featured item on the home page
+ */
 function getFeaturedId(){
 	$recipes = json_decode(file_get_contents(APP_PATH."data/recipes.json"), true);
 	return $recipes['featured'];
 }
 
+/**
+ * @param $instruction - The recipe instruction to parse
+ * @param $recipe - The recipe the instruction is located in
+ * @param $ingredients - List of all available ingredients
+ *
+ * @return string - The formatted HTML to show the instruction
+ */
 function parseRecipeInstruction($instruction, $recipe, $ingredients){
 	$pattern = '/({\\d+})/';
+
 	$replaceFunction = function ($match) use (&$ingredients, &$recipe){
 		$ingredientId = substr($match[0], 1, 1);
 		$ingredient = $ingredients[$ingredientId];
@@ -30,13 +44,14 @@ function parseRecipeInstruction($instruction, $recipe, $ingredients){
 				break;
 			}
 		}
-		$tooltopString = $ingredientAmount.$ingredient['unit'];
-		$tooltopContent = $ingredient['name'];
+		$tooltipString = $ingredientAmount.$ingredient['unit'];
+		$text = $ingredient['name'];
 		$tooltipHtml = "<a class='tooltip-btn' href='#'
-						data-toggle='tooltip' data-placement='top' title='$tooltopString'>
-						$tooltopContent</a>";
+						data-toggle='tooltip' data-placement='top' title='$tooltipString'>
+						$text</a>";
 		return ($tooltipHtml);
 	};
+
 	$parsedInstruction = preg_replace_callback($pattern, $replaceFunction, $instruction);
 
 	return $parsedInstruction;
