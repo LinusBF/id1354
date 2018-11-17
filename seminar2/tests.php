ec8441f93c91e86807d4a8df607d2cfa17d3d466
LinusBF <linus.webdevelopment@gmail.com>
2018-11-17 21:06:09 +0100

    Updated Seminar 2 APP_PATH

--- seminar2/tests.php
+++ seminar2/tests.php
@@ -6,7 +6,7 @@
  * Time: 20:32
  */
 DEFINE("LINK_PATH", getenv("PRODUCTION") !== false ? "/seminar2/" : "/id1354/seminar2/");
-DEFINE("APP_PATH", getenv( "PRODUCTION" ) !== false ? $_SERVER["DOCUMENT_ROOT"]."/seminar2/" : "/id1354/seminar2/");
+DEFINE("APP_PATH", getenv( "PRODUCTION" ) !== false ? $_SERVER["DOCUMENT_ROOT"]."/seminar2/" : str_replace("\\", "/", __DIR__)."/");
 
 include_once "./controllers/userController.php";
 
